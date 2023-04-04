a6aa82bebc86b7dd96ae653c5ec7b6f2c8bb79da
dmooredidit <dandre.moore2@gmail.com>
2023-04-04 15:37:52 -0700

    completed challenge

---
+++ ruby-intro-DeMario-DAndre.rb
@@ -0,0 +1,19 @@
+my_variable = 'kevin'
+puts "Hi! #{my_variable.upcase}"
+puts "Hi! #{my_variable.reverse}"
+puts "Hi! #{my_variable.include?('e')}"
+puts "Hi! #{my_variable.capitalize}"
+puts "Hi! #{my_variable.delete('n')}"
+puts "Hi! #{my_variable.index('v')}"
+puts "Hi! #{my_variable.swapcase}"
+
+my_array = ["M.A.S.H", 'Super Girl', 'Mean Girls', "Dawson's Creek", 'Twilight']
+puts my_array
+puts my_array.length
+puts my_array.first
+puts my_array[3]
+puts my_array.reverse!
+
+my_new_array = []
+puts my_new_array[0] = 'The Boys'
+puts my_new_array[1] = 'Invincible'
