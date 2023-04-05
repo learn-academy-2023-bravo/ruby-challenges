b07c9960e6b137cc8ed7487075f7ef95dd1ae15d
FLopez73 <flopez730@gmail.com>
2023-04-05 16:35:18 -0700

    4 questions left

---
+++ blocks-valeria-felix.rb
@@ -0,0 +1,69 @@
+# Create a loop that prints the numbers 1 to 20.
+# number =1 
+# while number <= 20 
+#     p number 
+#     number = number + 1
+# end
+
+# Create a loop that prints the numbers 1 to 20 using a different approach than previously used.
+# (1..20).each do |value|
+#     p value
+# end
+
+
+# Create a loop that prints only even numbers from 20 to 0.
+# num = 20 
+# while num > 0
+#     p num 
+#     num = num - 2
+# end
+
+
+
+# Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+# num_array = [2, 5, 7, 9, 12]
+# new_array = []
+
+# num_array.map do |value|
+#     new_array << value * 5
+# end
+# p new_array
+
+
+
+# Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+# low_cap_array = ['hello', 'world', 'how', 'are', 'you']
+
+# low_cap_array.map! do |value|
+#     value.capitalize
+# end
+ 
+# p low_cap_array
+
+    
+
+# Create a method that takes in an array of numbers and returns the largest number in the array.
+# nums_array = [1, 25, 65, 96, -12]
+
+# p nums_array.max()
+
+# Create a method that takes in an array of words and returns the word with the least number of characters.
+low_cap_array = ['hello', 'world', 'how', 'are', 'you', 'hi']
+
+# p low_cap_array.min{|a,b| a.size <=> b.size}
+
+
+# Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+nums_array = [1, 25, 65, 96, -12, 13, 73, 67]
+
+odds = 
+
+
+
+# Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercase letters should be returned lowercased and all lowercased letters should be returned in uppercase letters.
+
+
+# Create a method that will take in an array of numbers and return an array with the remainder of each number when divided by 2.
+
+
+# Create a method that will take in a range of letters and return an array with all the letters capitalized.
