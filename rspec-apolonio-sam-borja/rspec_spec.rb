c3242221ce1cc49b3c706efb693731763120f4ab
sam-saenz <samsaenz31@gmail.com>
2023-04-11 16:26:43 -0700

    Completed challenges

--- rspec-apolonio-sam-borja/rspec_spec.rb
+++ rspec-apolonio-sam-borja/rspec_spec.rb
@@ -1,8 +1,9 @@
 require 'rspec'
+require 'date'
 require_relative 'rspec'
+require_relative 'task_list'
 
 describe 'Task' do
-
     before :each do
         @task = Task.new
     end
@@ -26,13 +27,27 @@ describe 'Task' do
     it 'has a status that can be marked done' do
         expect(@task.status).to be_a String
         expect(@task.status).to eq 'in progress'
-        @task.status = 'completed'
+        @task.complete
         expect(@task.status).to be_a String
         expect(@task.status).to eq 'completed'
     end
 
     it 'can print its status' do
-        expect{@task.print_status}.to 
+        @task.title = 'Washing the car'
+        expect{@task.complete}.to change {@task.status}.from('in progress').to('completed')
+        expect(@task.complete).to be_a String
+        expect(@task.complete).to eq('Washing the car is completed')
+    end
 
+    it 'can give a task a due date' do
+        @task.due_date = Date.today + 10
+        expect(@task.due_date).to eq (Date.parse('2023-04-21'))
+    end
 end
 
+# describe 'TaskList' do
+#     it 'has to be real' do
+#         expect { TaskList.new }.not_to raise_error
+#     end
+
+# end
