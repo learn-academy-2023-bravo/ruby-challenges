8f3eb136e73568522656e253004d9b0d0dbf2b03
wagnergon <wagnergon52@gmail.com>
2023-04-11 15:56:50 -0700

    rspac

--- rspec-wags-spencer-dre/rspec.rb
+++ rspec-wags-spencer-dre/rspec.rb
@@ -1,33 +1,58 @@
 
 # As a developer, I can create a Task.
 
-class Task
-    attr_accessor :cleaning 
-    def initialize(cleaning)
-        @cleaning = cleaning
-    end
-end
+# class Task
+#     attr_accessor :cleaning 
+#     def initialize(cleaning)
+#         @cleaning = cleaning
+#     end
+# end
 
 
 
 # As a developer, I can give a Task a title and retrieve it.
-class Task
-    attr_accessor :cleaning, :laundry
-    def initialize(cleaning, laundry= 'laundry')
-        @cleaning = cleaning
-        @laundry = laundry
-    end
-end
+# class Task
+#     attr_accessor :cleaning, :laundry
+#     def initialize(cleaning, laundry= 'laundry')
+#         @cleaning = cleaning
+#         @laundry = 'false'
+#     end
+# end
 
 # As a developer, I can give a Task a description and retrieve it.
-
+# class Task
+#     attr_accessor :cleaning, :laundry
+#     def initialize(cleaning, laundry= 'laundry')
+#         @cleaning = cleaning
+#         @laundry = 'false'
+#     end
+# end
 
 # As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
+# class Task
+#     attr_accessor :title, :description, :status
+#     def initialize
+#         @title = title
+#         @description = description
+#         @status = 'in progress'
+#     end
+# end
+
 
 
 
 # As a developer, when I print a Task that is done, its status is shown.
 
-
+class Task
+    attr_accessor :title, :description, :status
+    def initialize
+        @title = title
+        @description = description
+        @status = 'in progress'
+    end
+    def task_done
+        @status = 'complete'
+    end 
+end
 
 # As a developer, I can give a Task a due date. Hint: Use the built-in Ruby Date 
--- rspec-wags-spencer-dre/rspec_spec.rb
+++ rspec-wags-spencer-dre/rspec_spec.rb
@@ -2,15 +2,48 @@ require 'rspec'
 require_relative 'rspec.rb'
 
 
-describe Task do
-    it  'can be created' do
-        expect { Task.new ('cleaning') }.to_not raise_error
+describe 'Task' do
+
+    before :each do
+        @task = Task.new
     end
-    it 'has a job' do 
-        task = Task.new('cleaning')
-        expect(task.cleaning).to eq('cleaning')
-        expect(task.cleaning).to be_a(String) 
+    
+    it  'can be created' do
+        expect { @task }.to_not raise_error
+    end
+    
+    it 'has a title' do 
+        @task.title = 'laundry'
+        expect(@task.title).to eq('laundry')
+        expect(@task.title).to be_a(String)
+    end
+    
+    it 'has a description' do
+        @task.description = 'fold clothes'
+        expect(@task.description).to eq('fold clothes')
+        expect(@task.description).to be_a(String)
     end
 
-    it 'can be done or not done' do
+    it 'has a status' do
+        @task.status = 'in progress'
+        expect(@task.status).to eq('in progress')
+        expect(@task.status).to be_a(String)
+    end
+    it 'can be complete' do
+        @task.task_done = 'complete'
+        
+
+
 end
+
+    
+    
+    
+
+
+
+
+
+
+
+
