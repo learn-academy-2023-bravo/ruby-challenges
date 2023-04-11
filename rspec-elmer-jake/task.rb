90289578820e9acc2909ad610ea553f7e4596c15
drfudd2001 <elmer.figueroagarcia@gmail.com>
2023-04-11 14:26:03 -0700

    well this is working

--- rspec-elmer-jake/task.rb
+++ rspec-elmer-jake/task.rb
@@ -1,6 +1,17 @@
-# class Task
-#     attr_accessor :title
-#     def initialize (title)
-#         @title = title
-#     end
-# end
+class Task
+    attr_accessor :title, :description, :status
+
+    def initialize(title = '', description = '')
+        @title = title
+        @description = description
+        @status = 'in progress'
+    end
+
+    def mark_done
+        @status = 'done'
+    end
+
+    def to_s
+        "Title: #{@title} | Description: #{@description} | Status: #{@status}"
+    end
+end
--- rspec-elmer-jake/task_spec.rb
+++ rspec-elmer-jake/task_spec.rb
@@ -1,16 +1,31 @@
 require 'rspec'
-require_relative 'task.rb'
+require_relative 'task'
 
-
-describe 'Task' do
-    it 'when a task is instantiated' do
-        expect { Task.new }.not_to raise_error
-    end
-    it 'has title' do
-        expect { Task.new }.not_to raise_error
+describe Task do
+    it 'creates a new task with default values' do
         task = Task.new
-        task.title = 'study'
-            expect (task.title).to be_a String
-            expect (task.title).to eq 'study'
+        expect(task).to be_a(Task)
+        expect(task.status).to eq('in progress')
+    end
+
+    it 'creates a new task with given title and description' do
+        task = Task.new('Study', 'Learn a new testing suite')
+        expect(task.title).to eq('Study')
+        expect(task.description).to eq('Learn a new testing suite')
+        expect(task.status).to eq('in progress')
+    end
+
+    it 'marks a task as done' do
+        task = Task.new
+        task.mark_done
+        expect(task.status).to eq('done')
+    end
+
+    it 'returns task details including status when printed' do
+        task = Task.new('Study', 'Learn a new testing suite')
+        expect(task.to_s).to eq('Title: Study | Description: Learn a new testing suite | Status: in progress')
+
+        task.mark_done
+        expect(task.to_s).to eq('Title: Study | Description: Learn a new testing suite | Status: done')
     end
 end
